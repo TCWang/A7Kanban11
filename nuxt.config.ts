6922afd26f1d9cb101491d35e483946c3e9ff53a
TC Wang <wangtct@hotmail.com>
2023-12-17 11:04:36 +0800

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,23 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/image-edge", "@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+  css: ["@/assets/css/tailwind.css"],
+
+  app: {
+    head: {
+      title: "A7社區網站-資訊匯總",
+      meta: [
+        {
+          name: "description",
+          content: "A7新林口-社區網站 V2.0 資訊匯總",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="mt-1">Test</h1>
+    <h1 class="mt-1">Test 資訊匯總</h1>
   </div>
 </template>
 
