2f16d79affb6b3454090dcc3b0d4669f78e10be1
TC Wang <wangtct@hotmail.com>
2023-12-17 10:45:01 +0800

    Updates

---
+++ _nuxt/index.c1baed5e.js
@@ -0,0 +1,1 @@
+import{_ as e,v as t,t as s,x as o}from"./entry.0d527fff.js";const c={setup(){return{}}},n=o("h1",{class:"mt-1"},"Test",-1),r=[n];function a(_,d,p,i,l,u){return s(),t("div",null,r)}const m=e(c,[["render",a]]);export{m as default};
